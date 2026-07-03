1cc00cd69d7d74549077eba3e94a6000b358b9fb
Sumeet Raj <sumeet.raj2222@gmail.com>
2026-07-03 19:21:00 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char operator;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);   // space before %c skips leftover newline
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    if (operator == '+') {
+        result = num1 + num2;
+        printf("Result: %.2lf\n", result);
+    }
+    else if (operator == '-') {
+        result = num1 - num2;
+        printf("Result: %.2lf\n", result);
+    }
+    else if (operator == '*') {
+        result = num1 * num2;
+        printf("Result: %.2lf\n", result);
+    }
+    else if (operator == '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+            printf("Result: %.2lf\n", result);
+        } else {
+            printf("Error: Division by zero is not allowed.\n");
+        }
+    }
+    else {
+        printf("Invalid operator entered.\n");
+    }
+
+    return 0;
+}
